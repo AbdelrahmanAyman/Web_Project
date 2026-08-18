250cf05df20add2e9ae52027fe120bc1460002a5
AbdelrahmanAyman <bedoayman233@gmail.com>
2026-08-19 01:10:57 +0300

    Update index.js

--- project's backend/index.js
+++ project's backend/index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const menuRouter = require("./routes/menuRoutes");
+const authRouter = require("./routes/authRoutes");
 const dbConnect = require("./comfig/db-connect");
 
 const app = express();
@@ -16,8 +17,9 @@ app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/menu", menuRouter);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
