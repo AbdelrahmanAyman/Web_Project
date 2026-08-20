f0ebda2598cf75625ae08aa0f27329a13bafa956
AbdelrahmanAyman <bedoayman233@gmail.com>
2026-08-20 22:54:44 +0300

    Update auth.js

--- project's backend/middleware/auth.js
+++ project's backend/middleware/auth.js
@@ -4,17 +4,18 @@ const User = require("../models/User");
 const protect = async (req, res, next) => {
   try {
     let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-      token = req.headers.authorization.split(" ")[1];
-    }
-
     if (!token) {
       return res.status(401).json({
         status: "fail",
         message: "You are not logged in. Please log in to get access.",
       });
     }
+    
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1];
+    }
 
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const currentUser = await User.findById(decoded.id);
 
